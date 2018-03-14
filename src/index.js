5c7137099b51f3b0bbbc18f238c48fc050fc549a
candentira <vburchyk@pentaho.com>
2018-03-14 18:03:45 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let bigArray = new Array(999999);
+  array.forEach(item => {
+    bigArray[item] = item;
+  });
+  let maxConsecutive = [];
+  let proceed = false;
+  let counter = 1;
+  let previous = -999;
+  bigArray.forEach(item => {
+    if(proceed) {
+      counter++;
+    } else {
+      maxConsecutive.push(counter);
+      counter = 1;
+    }
+    proceed = previous + 1 == item;
+    previous = item;
+  });
+  if(proceed){
+    maxConsecutive.push(counter);
+  } 
+  return Math.max(...maxConsecutive, 0);
 }
